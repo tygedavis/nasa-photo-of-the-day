1850634321d6e5a650c5a7fab4ed00c34d5cac0f
tygedavis <tyge.davis@gmail.com>
2019-11-06 14:23:40 -0700

    Added the data recieved by the NASA API to the effect hook. Added React import to the other files. Renamed 'Button.js' to 'ImageFooter.js'

--- src/App.js
+++ src/App.js
@@ -3,17 +3,23 @@ import "./App.css";
 import axios from "axios";
 
 function App() {
-  const [data, setData] = useState()
+  const [data, setData] = useState({})
 
 useEffect(() => {
-  axios.get()
+  axios.get('https://api.nasa.gov/planetary/apod?api_key=pDlfBcC5P5gf9EgXHQoRJydWMxdMxvHzpDnaBtKi')
+  .then(res => {
+    console.log(res.data)
+  })
+  .catch(err => {
+    console.log('There was an error in the Axios call: ', err)
+  })
 }, [])
 
   return (
     <div className="App">
       <p>
         Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
+        app! Have fun <span role='img' aria-label='rocket emoji'>🚀</span>!
       </p>
     </div>
   );
