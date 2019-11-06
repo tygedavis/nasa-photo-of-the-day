9b4987b84a31f4c3a869e2cd8eaf71c9be304d29
tygedavis <tyge.davis@gmail.com>
2019-11-06 13:47:57 -0700

    Made files needed for site, downloaded Axios, put state in App.js as well as began Axios call

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import "./App.css";
+import axios from "axios";
 
 function App() {
+  const [data, setData] = useState()
+
+useEffect(() => {
+  axios.get()
+}, [])
+
   return (
     <div className="App">
       <p>
