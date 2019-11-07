a23236ece087adb05ac0fe28fcb58454e5977e4f
tygedavis <tyge.davis@gmail.com>
2019-11-07 14:05:01 -0700

    Moved all of my styling from yesterday into the React folders using styled-components. Added some fonts as well

--- src/components/Heading.js
+++ src/components/Heading.js
@@ -1,10 +1,20 @@
 import React from "react";
+import styled from 'styled-components';
+
+const TitleH1 = styled.h1`
+    font-size: 64px;
+    font-family: 'Gugi', cursive;
+`;
+
+const SubHeading = styled.h3`
+    font-family: 'Raleway', sans-serif;
+`;
 
 function Heading(){
     return(
         <div>
-            <h1>NASA</h1>
-            <h3>A Picture A Day</h3>
+            <TitleH1>NASA</TitleH1>
+            <SubHeading>A Picture A Day</SubHeading>
         </div>
     )
 }
--- src/components/body-components/Image.js
+++ src/components/body-components/Image.js
@@ -1,14 +1,37 @@
 import React from "react";
+import styled from 'styled-components';
 
+const ImageDiv = styled.div`
+    font-family: 'Raleway', sans-serif;
+    box-sizing: border-box;
+    width: 40%;
+    display:flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    padding: 0 20px;
+    margin: 0 auto;
+`;
+
+const ImageH4 = styled.h4`
+
+    font-size: 25px;
+    margin: 5px 0;
+`;
+
+const AboutP = styled.p`
+
+    font-size: 20px;
+`;
 
 function Image(props) {
  return(
-     <div className='image-div'>
+     <ImageDiv>
         <h3>The Title of this Image: {props.title}</h3>
         <img className='space-image' alt='Space' src={props.url}/>
-        <h4>About this Image:</h4>
-        <p>{props.explanation}</p>
-    </div>
+        <ImageH4>About this Image:</ImageH4>
+        <AboutP>{props.explanation}</AboutP>
+    </ImageDiv>
  )
 };
 
